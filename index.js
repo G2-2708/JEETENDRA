a35d76f54b753e4a4fbc46ae8274250857719417
Test <abc@gmail.com>
2025-06-08 18:33:24 +0530

    Test

---
+++ index.js
@@ -0,0 +1,14 @@
+let express=require("express")
+
+let app=express()
+
+app.get("/",(req,res)=>{                    //Creating a route here 
+    res.send({status:1,msg:"Home Page API"})
+})
+
+app.get("/news",(req,res)=>{
+    res.send({status:1,msg:"News page API"})
+})
+
+
+app.listen("7000")
